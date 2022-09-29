9eb48d843da58aa6938bb9fa089e78af282c2067
redi961 <gkdlvj23@naver.com>
2022-09-29 17:53:30 +0900

    Style 관련 수정

--- 01/Script/exam.js
+++ 01/Script/exam.js
@@ -4,7 +4,7 @@ const show = () => {
 
   radios.style.display = 'none';
   baseMent.style.display = 'none';
-  
+
   // 라디오버튼 밸류값 추출
   let r_value = document.querySelector('input[class="diceRadio"]:checked').value;
 /*    for (let item of radios) {
@@ -17,15 +17,18 @@ const show = () => {
   
   const imgs = document.querySelector("#diceSection")
   const ran = Math.floor (Math.random()*6)+1
+  const bt = document.querySelector(".bt1")
+
    tag = tag + `<div class="divMsg"><img src="./image/${ran}.png" class = "msgImg"}"></div>` 
    imgs.innerHTML = tag 
    
 
     if (ran == r_value) {
       tag2 = tag2 + `<div class="divMsg"><img src="./image/o.png" class = "msgImg"}"></div>`
-      ment = ment + `<h2> 성공!! </h2>`
+      ment = ment + `<h2> 성공!!</h2>`
       console.log ("일치");
-    }else {
+
+    } else {
       tag2 = tag2 + `<div class="divMsg"><img src="./image/x.png" class = "msgImg"}"></div>`
       ment = ment + `<h2> 실패!! </h2>`
       console.log ("불일치");
@@ -33,6 +36,7 @@ const show = () => {
     
      imgs.innerHTML += tag2
      imgs.innerHTML += ment  
+     bt.innerText = "재시도"
 
     console.log(ran);
     console.log(r_value);
